e94965ada10055cc1fcf43a972f03f1da52a70a7
Glen Souza <glenster75@gmail.com>
2023-04-14 00:09:15 -0700

    bicep test

--- Bicep/dns.ps1
+++ Bicep/dns.ps1
@@ -1,23 +1,21 @@
-$HostName = "www.orsclab.com"
-
 $headers = @{
     "Authorization" = "Bearer $Env:APIKey"
     "Content-Type"  = "application/json"
 }
 
 $records = Invoke-RestMethod `
-  -Uri "https://api.cloudflare.com/client/v4/zones/$Env:ZoneId/dns_records?name=$HostName" `
+  -Uri "https://api.cloudflare.com/client/v4/zones/$Env:ZoneId/dns_records?name=www.orsclab.com" `
   -Headers $headers `
   -Method 'GET'
 
 $cnameRecords = $records.result | Where-Object { $_.Type -eq "CNAME" }
 
 if ($null -ne $cnameRecords) {
-  if ($cnameRecords.content -ne $HostName) {
+  if ($cnameRecords.content -ne $Env:HostName) {
     # Update CNAME
     $recordId = $cnameRecords.id
     $url = "https://api.cloudflare.com/client/v4/zones/$Env:ZoneId/dns_records/$recordId"
-    $body = @{ "content" = $HostName } | ConvertTo-Json
+    $body = @{ "content" = $Env:HostName } | ConvertTo-Json
     Invoke-RestMethod `
       -Uri $url `
       -Headers $headers `
@@ -30,7 +28,7 @@ else {
     $body = @{
       "type" = "CNAME"
       "name" = "www"
-      "content" = $HostName
+      "content" = $Env:HostName
       "ttl" = 300
       "proxied" = $False
     } | ConvertTo-Json
